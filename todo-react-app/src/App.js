f6bb1bf98ffb7e803bdf5067ea8ba525194242d0
Onthesly <jysi123@gmail.com>
2021-12-07 17:58:33 +0900

    day2-p181

---
+++ todo-react-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Todo from "./Todo";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: { id: 0, title: "Hello World 1", done: true },
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todo item={this.state.item} />
+      </div>
+    );
+  }
+}
+
+export default App;
