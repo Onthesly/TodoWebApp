310ffaa30e7e5d5517999fda8852cc7836de7ef5
Onthesly <jysi123@gmail.com>
2021-12-08 16:11:38 +0900

    day3-198p

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,4 +1,6 @@
+import { Container, List, Paper } from "@material-ui/core";
 import React from "react";
+import AddTodo from "./AddTodo";
 import "./App.css";
 import Todo from "./Todo";
 
@@ -6,14 +8,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: { id: 0, title: "Hello World 1", done: true },
+      items: [
+        { id: "0", title: "Hello World 1", done: true },
+        { id: "1", title: "Hello World 2", done: false },
+      ],
     };
   }
 
+  //(1) 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야 됨
+    console.log("items : ", this.state.items);
+  };
+
   render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    // (2) 함수 연결
     return (
       <div className="App">
-        <Todo item={this.state.item} />
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
       </div>
     );
   }
