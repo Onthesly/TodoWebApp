9b5b69415f3c702d92260c1a3b2d6e73d5580a15
Onthesly <jysi123@gmail.com>
2021-12-30 19:15:37 +0900

    for confirm

--- todo-react-app/src/app-config.js
+++ todo-react-app/src/app-config.js
@@ -4,6 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
+} else {
+  backendHost =
+    "http://prod-todo-backend-dns.ap-northeast-2.elasticbeanstalk.com/"; // 자기 주소로 바꾸기
 }
 
 export const API_BASE_URL = `${backendHost}`;
